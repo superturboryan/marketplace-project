b413f0aae652fec31eaabf0b5d617551dae02567
Daniel <dan.hackl@gmail.com>
2019-05-06 17:09:14 -0400

    Daniels small commit

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,24 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import ItemGallery from "./ItemGallery.jsx";
+import "./../css/gallery.css";
 
 class UnconnectedApp extends Component {
   render = () => {
     return (
       <div>
-        This is App!
-        <div>
-          <Link to={"/item/1234"}>To item id 1234</Link>
-        </div>
-        <div>
-          <Link to={"/item/5678"}>To item id 5678</Link>
-        </div>
-        <div>
-          <Link to={"/signup"}>Signup</Link>
-        </div>
-        <div>
-          <Link to={"/login"}>Login</Link>
-        </div>
+        <ItemGallery />
       </div>
     );
   };
