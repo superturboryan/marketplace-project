e2a3ccc3322b9681f6c2605e0384a366cd2b33d6
Kenton <kenrilea@gmail.com>
2019-05-06 21:15:18 -0400

    added CheckoutButton.jsx Cart.jsx ItemList.jsx ItemListElem.jsx

---
+++ frontend/src/components/ItemListElem.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import renderProfile from "./renderProfile";
+
+class UnconnectedItemListElem extends Component {
+  render = () => {
+    let price = this.props.item.price;
+    price = "$" + price.toLocaleString({ style: "currency" });
+    return (
+      <li>
+        <div>
+          <span>{this.props.item.title}</span>
+          <span>{price}</span>
+          <span>
+            <img height="50px" src={this.props.item.images[0]} />
+          </span>
+        </div>
+      </li>
+    );
+  };
+}
+let ItemListElem = connect()(UnconnectedItemListElem);
+export default ItemListElem;
