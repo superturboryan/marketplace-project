6570a1c1aae4402e4b3519e80b4c29ad6c18bc73
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-08 11:17:06 -0400

    Add /verify-cookie to use with auto-login on FE, change get-all-items to acccept search query

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,106 +3,106 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 class UnconnectedLogin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-      redirect: false
-    };
-  }
+   constructor(props) {
+      super(props);
+      this.state = {
+         username: "",
+         password: "",
+         redirect: false
+      };
+   }
 
-  componentWillReceiveProps = () => {
-    this.setState({
-      redirect: true
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    // req.body.username
-    // req.body.password
-    let data = new FormData();
-    data.append("username", this.state.username);
-    data.append("password", this.state.password);
-    console.log(data);
-
-    fetch("/login", {
-      method: "POST",
-      credentials: "include",
-      body: data
-    })
-      .then(response => {
-        return response.text();
-      })
-      .then(responseBody => {
-        let body = JSON.parse(responseBody);
-
-        if (!body.success) {
-          console.log(body);
-          this.props.dispatch({
-            type: "show-message",
-            message: "Login not successful. Please try again"
-          });
-          return;
-        }
-
-        console.log(body);
-        this.props.dispatch({
-          type: "show-message",
-          message: "Login successful!"
-        });
-        this.props.dispatch({
-          type: "logged-in",
-          toggle: true
-        });
+   componentWillReceiveProps = () => {
+      this.setState({
+         redirect: true
       });
-  };
+   };
 
-  handleUsername = event => {
-    this.setState({
-      username: event.target.value
-    });
-  };
+   handleSubmit = event => {
+      event.preventDefault();
+      // req.body.username
+      // req.body.password
+      let data = new FormData();
+      data.append("username", this.state.username);
+      data.append("password", this.state.password);
+      console.log(data);
 
-  handlePassword = event => {
-    this.setState({
-      password: event.target.value
-    });
-  };
+      fetch("http://localhost:4000/login", {
+         method: "POST",
+         credentials: "include",
+         body: data
+      })
+         .then(response => {
+            return response.text();
+         })
+         .then(responseBody => {
+            let body = JSON.parse(responseBody);
 
-  render = () => {
-    if (this.props.loggedIn || this.state.redirect) {
-      return <Redirect to="/" />;
-    }
+            if (!body.success) {
+               console.log(body);
+               this.props.dispatch({
+                  type: "show-message",
+                  message: "Login not successful. Please try again"
+               });
+               return;
+            }
 
-    return (
-      <div>
-        <h1>Login</h1>
-        <form onSubmit={this.handleSubmit} encType={"multipart/form-data"}>
-          <div>Username</div>
-          <input
-            type="text"
-            onChange={this.handleUsername}
-            value={this.state.username}
-          />
-          <div>Password</div>
-          <input
-            type="text"
-            onChange={this.handlePassword}
-            value={this.state.password}
-          />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  };
+            console.log(body);
+            this.props.dispatch({
+               type: "show-message",
+               message: "Login successful!"
+            });
+            this.props.dispatch({
+               type: "logged-in",
+               toggle: true
+            });
+         });
+   };
+
+   handleUsername = event => {
+      this.setState({
+         username: event.target.value
+      });
+   };
+
+   handlePassword = event => {
+      this.setState({
+         password: event.target.value
+      });
+   };
+
+   render = () => {
+      if (this.props.loggedIn || this.state.redirect) {
+         return <Redirect to="/" />;
+      }
+
+      return (
+         <div>
+            <h1>Login</h1>
+            <form onSubmit={this.handleSubmit} encType={"multipart/form-data"}>
+               <div>Username</div>
+               <input
+                  type="text"
+                  onChange={this.handleUsername}
+                  value={this.state.username}
+               />
+               <div>Password</div>
+               <input
+                  type="text"
+                  onChange={this.handlePassword}
+                  value={this.state.password}
+               />
+               <input type="submit" value="Submit" />
+            </form>
+         </div>
+      );
+   };
 }
 
 let mapStateToProps = state => {
-  return {
-    loggedIn: state.loggedIn
-  };
+   return {
+      loggedIn: state.loggedIn
+   };
 };
 
 let Login = connect(mapStateToProps)(UnconnectedLogin);
