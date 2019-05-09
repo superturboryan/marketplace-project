31ca0e44ddb961f1f36a77fdcb499f2c2c138a5e
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-09 16:08:08 -0400

    Minor image gallery styling

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,14 +4,13 @@ import ItemGallery from "./ItemGallery.jsx";
 import "./../css/gallery.css";
 
 class UnconnectedApp extends Component {
-  render = () => {
-    return (
-      <div>
-        <p>All Items:</p>
-        <ItemGallery />
-      </div>
-    );
-  };
+   render = () => {
+      return (
+         <div>
+            <ItemGallery />
+         </div>
+      );
+   };
 }
 
 let App = connect()(UnconnectedApp);
