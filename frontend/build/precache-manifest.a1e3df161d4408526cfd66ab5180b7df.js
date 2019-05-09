c6087d49d5391a4139cc87674b0ce043779005b3
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-09 19:44:06 -0400

    NPM RUN BUILD

---
+++ frontend/build/precache-manifest.a1e3df161d4408526cfd66ab5180b7df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee3d7e34c8be372e4a4680e91895bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f780d8178659245b0b8",
+    "url": "/static/css/main.b00478ec.chunk.css"
+  },
+  {
+    "revision": "5a7ca4e75da180e3f779",
+    "url": "/static/js/2.326f2d7c.chunk.js"
+  },
+  {
+    "revision": "1f780d8178659245b0b8",
+    "url": "/static/js/main.e118f0b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
