b6cc8eac1d17dae8d6b7f6d5226e03adf031ce0b
Ryan David Forsyth <forsyth.r@gmail.com>
2019-05-09 16:57:22 -0400

    More minor styling to image gallery. Added clear cart button to cart page

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -4,45 +4,55 @@ import ItemList from "./ItemList.jsx";
 import CheckoutButton from "./CheckoutButton.jsx";
 
 class UnconnectedCart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [] };
-  }
-  componentWillMount = () => {
-    //______________________________________
+   constructor(props) {
+      super(props);
+      this.state = { items: [] };
+   }
 
-    fetch("http://localhost:4000/get-cart", { credentials: "include" })
-      .then(res => {
-        return res.text();
-      })
-      .then(resBody => {
-        let cartFromServer = JSON.parse(resBody);
-        console.log("cartFromServer");
-        console.log(cartFromServer);
-        this.setState({ items: cartFromServer });
-        this.props.dispatch({ type: "clear-cart" });
-        cartFromServer.forEach(item => {
-          this.props.dispatch({
-            type: "add-to-cart",
-            item: item,
-            quantity: item.quantity
-          });
-        });
-      });
-    //______________________________________
-  };
-  handlerOnClick = () => {};
-  render = () => {
-    return (
-      <div>
-        <ItemList allItems={this.state.items} />
-        <CheckoutButton />
-      </div>
-    );
-  };
+   clearCart = () => {
+      fetch("http://localhost:4000/clear-cart", { credentials: "include" })
+         .then(
+            this.props.dispatch({ type: "clear-cart" })
+         )
+      this.setState({ items: [] })
+   }
+
+   componentWillMount = () => {
+      //______________________________________
+
+      fetch("http://localhost:4000/get-cart", { credentials: "include" })
+         .then(res => {
+            return res.text();
+         })
+         .then(resBody => {
+            let cartFromServer = JSON.parse(resBody);
+            console.log("cartFromServer");
+            console.log(cartFromServer);
+            this.setState({ items: cartFromServer });
+            this.props.dispatch({ type: "clear-cart" });
+            cartFromServer.forEach(item => {
+               this.props.dispatch({
+                  type: "add-to-cart",
+                  item: item,
+                  quantity: item.quantity
+               });
+            });
+         });
+      //______________________________________
+   };
+   handlerOnClick = () => { };
+   render = () => {
+      return (
+         <div>
+            <ItemList allItems={this.state.items} />
+            <button onClick={this.clearCart}>Clear cart!</button>
+            <button> <CheckoutButton /> </button>
+         </div>
+      );
+   };
 }
 let mapStateToProps = state => {
-  return { cartItems: state.cart };
+   return { cartItems: state.cart };
 };
 let Cart = connect(mapStateToProps)(UnconnectedCart);
 export default Cart;
--- frontend/src/components/ItemDetails.jsx
+++ frontend/src/components/ItemDetails.jsx
@@ -9,99 +9,100 @@ import TabbedImageGallery from "./TabbedImageGallery.jsx";
 import "./../css/itemDetails.css";
 
 class UnconnectedItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      item: undefined
-    };
-    console.log("Initial state: ");
-    console.log(this.state);
-  }
+   constructor(props) {
+      super(props);
+      this.state = {
+         item: undefined
+      };
+      console.log("Initial state: ");
+      console.log(this.state);
+   }
 
-  componentDidMount = () => {
-    // Fetch item details from server
-    fetch("/get-single-item?itemId=" + this.getItemId())
-      .then(response => {
-        return response.text();
-      })
-      .then(responseBody => {
-        let body = JSON.parse(responseBody);
+   componentDidMount = () => {
+      // Fetch item details from server
+      fetch("/get-single-item?itemId=" + this.getItemId())
+         .then(response => {
+            return response.text();
+         })
+         .then(responseBody => {
+            let body = JSON.parse(responseBody);
 
-        if (body.success === false) {
-          console.log("Item does not exist.", body);
-          return;
-        }
+            if (body.success === false) {
+               console.log("Item does not exist.", body);
+               return;
+            }
 
-        console.log("Received item details: ", body);
-        this.setState({
-          item: body
-        });
-      });
-  };
+            console.log("Received item details: ", body);
+            this.setState({
+               item: body
+            });
+         });
+   };
 
-  getItemId = () => {
-    let temp = this.props.location.pathname;
-    return temp.substring(temp.lastIndexOf("/") + 1);
-  };
+   getItemId = () => {
+      let temp = this.props.location.pathname;
+      return temp.substring(temp.lastIndexOf("/") + 1);
+   };
 
-  getCityStr = () => {
-    let item = this.state.item;
-    return item.city + ", " + item.province + ", " + item.country;
-  };
+   getCityStr = () => {
+      let item = this.state.item;
+      return item.city + ", " + item.province + ", " + item.country;
+   };
 
-  render() {
-    console.log("ItemDetailsCompenent props: ", this.props.match.params.id);
+   render() {
+      console.log("ItemDetailsCompenent props: ", this.props.match.params.id);
 
-    if (this.state.item === undefined) {
-      return null;
-    }
+      if (this.state.item === undefined) {
+         return null;
+      }
 
-    return (
-      <div className="item-details-container">
-        <TabbedImageGallery
-          images={this.state.item.images}
-          itemId={this.state.item.id}
-          title={this.state.item.title}
-        />
-        <div className="item-details-details-container">
-          <h1>{this.state.item.title}</h1>
-          <Link
-            className="item-details-link-to-seller"
-            to={"/profile/" + this.state.item.userId}
-          >
-            Link to seller
+      return (
+         <div className="item-details-container">
+
+            <div className="item-details-details-container">
+               <h1>{this.state.item.title}</h1>
+               <Link
+                  className="item-details-link-to-seller"
+                  to={"/profile/" + this.state.item.userId}
+               >
+                  Link to seller
           </Link>
-          <p className="item-details-description">{this.state.item.details}</p>
-          <fieldset>
-            <dl>
-              <dt>Price</dt>
-              <dd>
-                $
+               <p className="item-details-description">{this.state.item.details}</p>
+               <fieldset>
+                  <dl>
+                     <dt>Price</dt>
+                     <dd>
+                        $
                 {parseFloat(this.state.item.price).toLocaleString({
-                  style: "currency"
-                })}
-              </dd>
-            </dl>
-            <dl>
-              <dt>In Stock</dt>
-              <dd>{this.state.item.stock} available</dd>
-            </dl>
-            <dl>
-              <dt>Ships from</dt>
-              <dd>{this.getCityStr()}</dd>
-            </dl>
-          </fieldset>
-          <AddToCart item={this.state.item} />
-        </div>
-        <div className="item-details-review-container">
-          <AddReview itemId={this.state.item.itemId} />
-        </div>
-        <div className="item-details-reviews">
-          <ReviewList itemId={this.state.item.itemId} />
-        </div>
-      </div>
-    );
-  }
+                           style: "currency"
+                        })}
+                     </dd>
+                  </dl>
+                  <dl>
+                     <dt>In Stock</dt>
+                     <dd>{this.state.item.stock} available</dd>
+                  </dl>
+                  <dl>
+                     <dt>Ships from</dt>
+                     <dd>{this.getCityStr()}</dd>
+                  </dl>
+               </fieldset>
+               <AddToCart item={this.state.item} />
+            </div>
+            <TabbedImageGallery
+               images={this.state.item.images}
+               itemId={this.state.item.id}
+               title={this.state.item.title}
+            />
+            <div className="item-details-review-container">
+               <AddReview itemId={this.state.item.itemId} />
+            </div>
+            <div className="item-details-reviews">
+               <ReviewList itemId={this.state.item.itemId} />
+            </div>
+         </div>
+      );
+   }
 }
 
 let ItemDetails = connect()(UnconnectedItem);
