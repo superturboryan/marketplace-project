0841f29b902d647bc0e3cfbdf71be7a600ad4eaa
Kenton <kenrilea@gmail.com>
2019-05-09 14:13:13 -0400

    resolved conflict

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -6,33 +6,12 @@ import Searchbar from "./Searchbar.jsx";
 import "./../css/navigationBar.css";
 
 class UnconnectedNavigationBar extends Component {
-   ifLoggedInDoThis = returnThis => {
-      if (this.props.loggedIn) {
-         return returnThis;
-      }
-   };
+  ifLoggedInDoThis = returnThis => {
+    if (this.props.loggedIn) {
+      return returnThis;
+    }
+  };
 
-<<<<<<< HEAD
-   getButtonHtml = () => {
-      if (!this.props.loggedIn) {
-         return (
-            <div>
-               <div>
-                  <Link to={"/signup"}>Signup</Link>
-               </div>
-               <div>
-                  <Link to={"/login"}>Login</Link>
-               </div>
-            </div>
-         );
-      }
-   };
-
-   getNavigationLinks = () => {
-      return (
-         <div className="navigation-flex link-area just-bottom">
-            :
-=======
   getSignupLogin = () => {
     if (!this.props.loggedIn) {
       return (
@@ -66,46 +45,15 @@ class UnconnectedNavigationBar extends Component {
     return (
       <div className="navigation-flex link-area just-bottom">
         Categories:
->>>>>>> 16438fad3a4a03399bd3e2ffce2d628123754e32
         <div>Clothing and Accessories</div>
-            <div>Films and Music</div>
-            <div>Home and Appliances</div>
-            <div>Electronics</div>
-            <div>Video Games</div>
-         </div>
-      );
-   };
+        <div>Films and Music</div>
+        <div>Home and Appliances</div>
+        <div>Electronics</div>
+        <div>Video Games</div>
+      </div>
+    );
+  };
 
-<<<<<<< HEAD
-   render = () => {
-      return (
-         <div className="navigation-bar">
-            <div className="navigation-flex">
-               <div className="navigation-unflex">
-                  <Link to={"/"}>Ali-Bae</Link>
-               </div>
-               <Searchbar />
-               {this.getButtonHtml()}
-               {this.ifLoggedInDoThis(
-                  <div className="toTheEnd navigation-unflex">
-                     <div>
-                        <span>{this.props.username + " |"}</span>
-                        <Link to={"/cart"}>My Cart</Link>
-                     </div>
-                     <Link className="toTheEnd" to={"/add-item"}>
-                        Sell Something!
-              </Link>
-                  </div>
-               )}
-            </div>
-            <div className="navigation-left-right">
-               {this.getNavigationLinks()}
-               {this.ifLoggedInDoThis(<Logout className="toTheEnd" />)}
-            </div>
-         </div>
-      );
-   };
-=======
   render = () => {
     return (
       <div className="navigation-bar">
@@ -124,14 +72,13 @@ class UnconnectedNavigationBar extends Component {
       </div>
     );
   };
->>>>>>> 16438fad3a4a03399bd3e2ffce2d628123754e32
 }
 
 let mapStateToProps = state => {
-   return {
-      loggedIn: state.loggedIn,
-      username: state.username
-   };
+  return {
+    loggedIn: state.loggedIn,
+    username: state.username
+  };
 };
 
 let NavigationBar = connect(mapStateToProps)(UnconnectedNavigationBar);
